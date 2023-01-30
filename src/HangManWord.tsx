72ad5559470a81ca1a74cce16d671d837db259a4
osama7007 <saadosama43@gmail.com>
2023-01-30 14:14:31 +0200

    upload hang man

---
+++ src/HangManWord.tsx
@@ -0,0 +1,41 @@
+type HangManWordProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+    reveal?: boolean
+  }
+  
+  export function HangManWord({
+    guessedLetters,
+    wordToGuess,
+    reveal = false,
+  }: HangManWordProps) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          gap: ".25em",
+          fontSize: "6rem",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+          fontFamily: "monospace",
+        }}
+      >
+        {wordToGuess.split("").map((letter, index) => (
+          <span style={{ borderBottom: ".1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility:
+                  guessedLetters.includes(letter) || reveal
+                    ? "visible"
+                    : "hidden",
+                color:
+                  !guessedLetters.includes(letter) && reveal ? "red" : "black",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        ))}
+      </div>
+    )
+  }
